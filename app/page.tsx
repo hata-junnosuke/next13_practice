02bf7b7d3262a620d6a0339923406b4dbd2e2e1e
hata-junnosuke <jh19911209@icloud.com>
2023-04-30 09:16:33 +0900

    11完了

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
 import NotesList from "./components/notes-list";
+import TimerCounter from "./components/timer-counter";
+import { Suspense } from "react";
+import Spinner from "./components/spinner";
+import RefreshBtn from "./components/refresh-btn";
 
 export default function Page() {
   return (
     <main>
       <div className="m-10 text-center">Hello World🚀</div>
-      {/* ベータ版のため指摘がvscode上で表示されるので以下のコメントで回避する */}
-      {/* @ts-ignore */}
-      <NotesList />
+      {/* Suspenseで囲むことでレンダリングを待たずしてそのほかを表示できる */}
+      <Suspense fallback={<Spinner color="border-green-500" />}>
+        {/* ベータ版のため指摘がvscode上で表示されるので以下のコメントで回避する */}
+        {/* @ts-ignore */}
+        <NotesList />
+      </Suspense>
+      {/* クライアントコンポーネント */}
+      <TimerCounter />
+      <RefreshBtn />
     </main>
   )
 }
